35ee06433237ac78cb87ea2843ae49f7fb3f354a
Christina Parassiadis <christinaparassiadis@Christinas-MacBook-Air-2.local>
2024-01-23 17:23:34 +0100

    added new things

--- scripts/my_first_code.R
+++ scripts/my_first_code.R
@@ -24,3 +24,56 @@ weight_kg <- sqrt(9)
 
 round(3.14159, digits=2)
 args(round) # gives information about the function 
+
+# vector is a series of elements, all in one type, use c()
+# don't mix numeric, character and logical 
+# use " to indicate a word, otherwise it could be recognized as an object 
+weight_g <- c(50, 60, 65, 82)
+animals <- c("mouse", "rat", "dog")
+
+# length of vector and type of data
+length(animals)
+class(animals)
+class(weight_g)
+
+# structure of the object
+str(animals)
+
+# add element to a vector
+animals <- c("cat", animals)
+animals <- c(animals, "frog")
+typeof(animals)
+
+# subsetting a vector
+animals[2]
+animals[c(1,2)]
+more_animals <- animals[c(1,2,3,2,1,4)]
+
+# operators: <, >, equal ==, different !=, <=, >=
+# combining with and &, or |
+# ! reverse a logical 
+
+weight_g[c(FALSE, FALSE, TRUE, TRUE)]
+weight_g > 63
+weight_g[weight_g > 63]
+weight_g[weight_g >63 & weight_g <80]
+weight_g[weight_g < 58 | weight_g > 80]
+weight_g==65
+
+animals[animals == "rat" | animals == "frog"]
+
+# %in% helps us find all elements corresponding to a vector of choice 
+animals %in% c("rat", "frog", "cat", "dog", "duck")
+
+# an example of a vector with missing data
+heights <- c(2,4,4,NA,6)
+mean(heights)
+mean(heights, na.rm=TRUE)
+# na.rm to remove NA values 
+max(heights, na.rm=T)
+is.na(heights)
+heights[!is.na(heights)]
+# omit the missing data
+na.omit(heights)
+# extract the complete cases
+heights[complete.cases(heights)]
