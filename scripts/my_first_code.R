347ecdfc1f787f0a8ff15193e4ca6e920823f384
Christina Parassiadis <christinaparassiadis@Christinas-MacBook-Air-2.local>
2024-01-23 15:01:38 +0100

    this is my first R script

---
+++ scripts/my_first_code.R
@@ -0,0 +1,14 @@
+weight_kg <- 55 
+# assignment operator for mac: alt - 
+# explicit names for objects for reproducibility, objectnames cannot start with a number, 
+# no dots on variable names, use underscore 
+# write command in brackets to get the result in the console i.e. (weight_kg <- 55)
+
+(2.2*weight_kg)
+
+weight_lb <- 2.2*weight_kg
+weight_kg <- 100
+# if you update a value, you have to rerun other commands again, no automatic updating
+
+# red titles are unsafed files 
+
