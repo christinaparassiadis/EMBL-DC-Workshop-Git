e063a614ccd1d4abc4ab212cbe20d990cadad8e7
Christina Parassiadis <christinaparassiadis@Christinas-MacBook-Air-2.local>
2024-01-23 15:52:07 +0100

    added code lines

--- scripts/my_first_code.R
+++ scripts/my_first_code.R
@@ -1,4 +1,5 @@
 weight_kg <- 55 
+# cmd enter for running
 # assignment operator for mac: alt - 
 # explicit names for objects for reproducibility, objectnames cannot start with a number, 
 # no dots on variable names, use underscore 
@@ -11,4 +12,15 @@ weight_kg <- 100
 # if you update a value, you have to rerun other commands again, no automatic updating
 
 # red titles are unsafed files 
+# always use tab complete to write something, no typos 
 
+mass <- 47.5
+age <- 122
+mass <- mass*2
+age <- age-20 
+mass_index <- mass/age
+
+weight_kg <- sqrt(9)
+
+round(3.14159, digits=2)
+args(round) # gives information about the function 
