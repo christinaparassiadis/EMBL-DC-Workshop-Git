06258ca93a95d16cd617c081c187d980cb243f32
Christina Parassiadis <christinaparassiadis@Christinas-MacBook-Air-2.local>
2024-01-24 10:07:05 +0100

    Did new things

--- scripts/data_frames.R
+++ scripts/data_frames.R
@@ -27,3 +27,38 @@ survey_200 <- survey[200,]
 survey[nrow(survey),]
 nrow(survey)/2
 survey[nrow(survey)/2,]
+
+# Tools, Global options, never reload workspace, better to run script again 
+
+# Factors
+str(survey)
+survey$sex <- factor(survey$sex)
+levels(survey$sex)
+nlevels(survey$sex)
+sex <- factor(c("male", "female", "female", "male"))
+sex <- factor(sex, levels = c("male", "female"))
+
+sum(survey$taxa == "rabbit")
+summary(survey$taxa)
+
+# convert factors 
+as.character(sex)
+
+year_fct <- factor(c(1990,1983,1977,1997))
+as.numeric(year_fct)
+as.numeric(as.character(year_fct))
+as.numeric(levels(year_fct))[year_fct]
+
+# Renaming factors 
+plot(survey$sex)
+summary(survey$sex)
+sex <- survey$sex
+levels(sex)
+sex <- addNA(sex)
+levels(sex)[3] <- "undetermined"
+levels(sex)
+plot(sex)
+levels(sex)[1:2] <- c("female", "male")
+plot(sex)
+sex <- factor(sex, levels=c("undeterminded", "female", "male"))
+plot(sex)
